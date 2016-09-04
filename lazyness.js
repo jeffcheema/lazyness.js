d455e36a82c27e45bb3647daf6b30adab602e26d
Jeff Cheema <jeffcheema007@gmail.com>
2016-09-04 15:26:42 -0700

    Update lazyness.js

--- lazyness.js
+++ lazyness.js
@@ -10,3 +10,13 @@ function qs(param){
 var qsfunc =document.querySelector(param);
 return qsfunc
 }
+function shortenurl(url){
+    $.getJSON('  https://api-ssl.bitly.com/v3/shorten?access_token=5e56641ffce09ad21fd0d0481d767c8267bd7819&longUrl='+encodeURIComponent(url)+'&format=json',
+
+    function(response) {
+      return response.data.url
+   
+    }
+
+);
+}
