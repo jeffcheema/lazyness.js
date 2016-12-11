b56bcb33da6d1f2277379d2a5d6a9db4fd443952
Jeff Cheema <jeffcheema007@gmail.com>
2016-12-10 21:12:39 -0800

    Update lazyness.js

--- lazyness.js
+++ lazyness.js
@@ -2,21 +2,47 @@ String.prototype.replaceAll = function(search, replacement) {
     var target = this;
     return target.replace(new RegExp(search, 'g'), replacement);
 };
-function qsa(param){
-var qsafunc =document.querySelectorAll(param);
-return qsafunc
-}
-function qs(param){
-var qsfunc =document.querySelector(param);
-return qsfunc
-}
-function shortenurl(url){
-    $.getJSON('  https://api-ssl.bitly.com/v3/shorten?access_token=5e56641ffce09ad21fd0d0481d767c8267bd7819&longUrl='+encodeURIComponent(url)+'&format=json',
 
-    function(response) {
-      return response.data.url
-   
-    }
+$= function(param){
+var qsfunc =document.querySelectorAll(param);
+  if (qsfunc.length == 1){
+     qsfunc =document.querySelector(param);
+    return qsfunc;
+  }
+  else{
+     qsfunc =document.querySelectorAll(param);
+    return qsfunc;
+    
+  }
 
-);
+}
+function loadScript(url, callback)
+{
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    head.appendChild(script);
+}
+var delay = ( function() {
+    var timer = 0;
+    return function(callback, ms) {
+        clearTimeout (timer);
+        timer = setTimeout(callback, ms);
+    };
+})();
+Element.prototype.remove = function() {
+this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+for(var i = this.length - 1; i >= 0; i--) {
+if(this[i] && this[i].parentElement) {
+this[i].parentElement.removeChild(this[i]);
+}
+}
 }
